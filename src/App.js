0bb0fd2c6aa1507acad49d974d4f541126732920
Ömer Ayhan <om.ayhan247@gmail.com>
2021-11-21 05:28:56 +0300

    docs: :memo: comment explanation for App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,13 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   const renderCards = ({ item }) => <CardItem cards={item} />;
+  // global state for basket items
   const [basketItems, setBasketItems] = useState([]);
+  // global state for main patistore data
   const [shopItems, setShopItems] = useState(patistore_data);
 
   return (
+    // provider will help us to use global states in all the child components
     <GlobalContext.Provider
       value={{
         basketState: { basketItems, setBasketItems },
@@ -23,11 +26,11 @@ export default function App() {
           <Header />
           <SearchBar />
           <FlatList
-            numColumns={2}
-            horizontal={false}
-            keyExtractor={({ id }) => id.toString()}
-            data={shopItems}
-            renderItem={renderCards}
+            numColumns={2} // 2 cards in a row
+            horizontal={false} // must be if numColumns is used
+            keyExtractor={({ id }) => id.toString()} // keyExtractor is used to identify each item
+            data={shopItems} // source of the FlatList
+            renderItem={renderCards} // renderCards is used to render each item
           />
         </View>
       </SafeAreaProvider>
